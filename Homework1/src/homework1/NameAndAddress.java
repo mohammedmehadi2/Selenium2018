924488753ca99d4b1807dfa507aa0f51b133363e
Mohammed Mehadi <mohammedmehadi2@gmail.com>
2018-11-28 11:35:41 -0500

    Homework2

---
+++ Homework1/src/homework1/NameAndAddress.java
@@ -0,0 +1,11 @@
+package homework1;
+
+public class NameAndAddress {
+	public static void main(String[]arg) {
+		System.out.println("Mohammed Mehadi");
+		System.out.println("555 Hemlock st");
+		System.out.println("Brooklyn, NY, 11208");
+	
+}
+
+}
